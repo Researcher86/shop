bcc001b012d230482d4d8b96f410da29bc07562a
Tanat <researcher86@mail.ru>
2015-10-05 09:16:37 +0600

    Рефакторинг теста

--- src/test/java/com/tanat/shop/domain/model/ShopServiceTest.java
+++ src/test/java/com/tanat/shop/domain/model/ShopServiceTest.java
@@ -1,71 +1,64 @@
 package com.tanat.shop.domain.model;
 
+import org.junit.Before;
 import org.junit.Test;
 
-import java.util.Calendar;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 
 /**
  * Created by Tanat on 02.10.2015.
  */
 public class ShopServiceTest {
 
+    private Client tanat;
+    private Goods pencel;
+    private Goods pencel2;
+
+    @Before
+    public void setUp() throws Exception {
+        tanat = new Client("Альпенов Танат Маратович", "87011520885", "Дощанова 133б");
+        pencel = new Goods("Ручка", 5, "Обычная", "Обычная ручка", new byte[5]);
+        pencel2 = new Goods("Ручка2", 5, "Обычная", "Обычная ручка", new byte[5]);
+    }
+
     @Test
     public void testClientByGoods() throws Exception {
-        Client tanat = new Client("Альпенов Танат Маратович", "87011520885", "Дощанова 133б");
-        Client janara = new Client("Альпенова Жанара Ериковна", "87015216343", "Дощанова 133б");
+        ShopService shopService = new ShopService();
+        shopService.clientByGoods(tanat, pencel, 3);
+        shopService.clientByGoods(tanat, pencel2, 2);
 
-        Goods pencel = new Goods("Ручка", 5, "Обычная", "Обычная ручка", new byte[5]);
-        Goods pencel2 = new Goods("Ручка2", 5, "Обычная", "Обычная ручка", new byte[5]);
+        Cart tanatCart = shopService.getCartByClient(tanat);
+        for (Order order : tanatCart.getOrders()) {
+            assertNotNull("Order is nul", order);
+            assertNotNull("Goods is nul", order.getGoods());
+            assertTrue("GoodsCount is 0", order.getGoodsCount() > 0);
+            assertTrue("Goods price is 0", order.getGoods().getPrice() > 0);
+            assertTrue("Order total price is 0", order.getTotalPrice() > 0);
+        }
 
+        assertTrue("Cart total price error", tanatCart.getTotalPrice() == 25);
+    }
+
+    @Test
+    public void testCategoryOfGoods() throws Exception {
         CategoryOfGoods categoryOfGoods = new CategoryOfGoods("Канцтовары");
         categoryOfGoods.addGoods(pencel);
         categoryOfGoods.addGoods(pencel2);
 
         for (Goods goods : categoryOfGoods.getGoods()) {
-            System.out.println(goods.getName() + " - " + goods.getCategoryOfGoods().getName());
+            assertNotNull("Goods is null", goods);
         }
+    }
 
-        System.out.println("Comment*******************************");
-
+    @Test
+    public void testCommentForGoods() throws Exception {
         Comment tanatComment = new Comment("Супер!", pencel, tanat);
-        Comment janaraComment = new Comment("Супер! Супер!", pencel2, janara);
 
-        System.out.println(tanatComment.getClient().getFio() + " " +
-                tanatComment.getGoods().getName() + " " +
-                tanatComment.getText() + " " +
-                tanatComment.getDate().getTime());
-
-        System.out.println(janaraComment.getClient().getFio() + " " +
-                janaraComment.getGoods().getName() + " " +
-                janaraComment.getText() + " " +
-                janaraComment.getDate().getTime());
-
-        ShopService shopService = new ShopService();
-        shopService.clientByGoods(tanat, pencel, 3);
-        shopService.clientByGoods(tanat, pencel2, 2);
-        shopService.clientByGoods(janara, pencel2, 2);
-
-        System.out.println("Cart*******************************");
-
-        Cart tanatCart = shopService.getCartByClient(tanat);
-        for (Order order : tanatCart.getOrders()) {
-            System.out.println("Name: " + order.getGoods().getName() +
-                    ", count: " + order.getGoodsCount() +
-                    ", price: " + order.getGoods().getPrice() +
-                    ", sum: " + order.getTotalPrice());
-        }
-        System.out.println("Tanat cart price: " + tanatCart.getTotalPrice());
-
-        System.out.println("Cart*******************************");
-
-        Cart janaraCart = shopService.getCartByClient(janara);
-        for (Order order : janaraCart.getOrders()) {
-            System.out.println("Name: " + order.getGoods().getName() +
-                    ", count: " + order.getGoodsCount() +
-                    ", price: " + order.getGoods().getPrice() +
-                    ", sum: " + order.getTotalPrice());
-        }
-        System.out.println("janara cart price: " + janaraCart.getTotalPrice());
-
+        assertNotNull("Comment client is null", tanatComment.getClient());
+        assertNotNull("Comment goods is null", tanatComment.getGoods());
+        assertNotNull("Comment text is null", tanatComment.getText());
+        assertNotNull("Comment date is null", tanatComment.getDate());
     }
 }
