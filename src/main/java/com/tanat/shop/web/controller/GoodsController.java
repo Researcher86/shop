37a2b7517e553a5135c27f1762559f1b2ee5ca4a
Tanat <researcher86@mail.ru>
2015-11-18 15:08:26 +0600

    Настройка логгера

--- src/main/java/com/tanat/shop/web/controller/GoodsController.java
+++ src/main/java/com/tanat/shop/web/controller/GoodsController.java
@@ -1,6 +1,8 @@
 package com.tanat.shop.web.controller;
 
 import com.tanat.shop.service.GoodsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,17 +17,21 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "/goods")
 public class GoodsController {
 
+    private Logger logger = LoggerFactory.getLogger(getClass().getName());
+
     @Autowired
     private GoodsService goodsService;
 
     @RequestMapping(method = RequestMethod.GET)
     public String index(Model model) {
+        logger.info("goodsAll");
         model.addAttribute("goodsAll", goodsService.getAll());
         return "/goods/all";
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     public String getById(@PathVariable("id") Long id, Model model) {
+        logger.info("getById {}", id);
         model.addAttribute("goods", goodsService.getById(id));
         return "/goods/goods";
     }
