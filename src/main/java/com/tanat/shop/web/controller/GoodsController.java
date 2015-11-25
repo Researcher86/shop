b991a5492ceabad229570ed10667166b30e5ee5b
Tanat <researcher86@mail.ru>
2015-11-25 16:52:15 +0600

    Добавил методы добавления и редактирования товаров + подключил биндинг для категории товаров

--- src/main/java/com/tanat/shop/web/controller/GoodsController.java
+++ src/main/java/com/tanat/shop/web/controller/GoodsController.java
@@ -1,14 +1,18 @@
 package com.tanat.shop.web.controller;
 
+import com.tanat.shop.model.Category;
+import com.tanat.shop.model.Goods;
+import com.tanat.shop.service.CategoryService;
 import com.tanat.shop.service.GoodsService;
+import com.tanat.shop.web.utils.CategoryEditor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Tanat on 16.11.2015.
@@ -21,6 +25,9 @@ public class GoodsController {
 
     @Autowired
     private GoodsService goodsService;
+
+    @Autowired
+    private CategoryService categoryService;
 
     @RequestMapping(method = RequestMethod.GET)
     public String index(Model model) {
@@ -38,5 +45,33 @@ public class GoodsController {
         model.addAttribute("goods", goodsService.getById(id));
 
         return "/goods/goods";
+    }
+
+    @RequestMapping(value = "/edit", method = RequestMethod.GET)
+    public String edit(@ModelAttribute Goods goods, Model model) {
+        model.addAttribute("categories", categoryService.getAll());
+        return "/goods/edit";
+    }
+
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public String edit2(@PathVariable Long id, Model model) {
+        model.addAttribute("goods", goodsService.getById(id));
+        model.addAttribute("categories", categoryService.getAll());
+        return "/goods/edit";
+    }
+
+    @RequestMapping(value = "/edit", method = RequestMethod.POST)
+    public String save(@ModelAttribute Goods goods /*, BindingResult bindingResult, Model model*/) {
+
+        goodsService.save(goods);
+        return "redirect:/goods";
+    }
+
+    @Autowired
+    private CategoryEditor categoryEditor;
+
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(Category.class, this.categoryEditor);
     }
 }
