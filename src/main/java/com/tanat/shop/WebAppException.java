472bfe4a9b67292eadb556f6c02523573583890c
Tanat <researcher86@mail.ru>
2015-11-25 14:19:44 +0600

    Добавил Exception для приложения

---
+++ src/main/java/com/tanat/shop/WebAppException.java
@@ -0,0 +1,12 @@
+package com.tanat.shop;
+
+/**
+ * Created by Tanat on 25.11.2015.
+ */
+public class WebAppException extends RuntimeException {
+
+    public WebAppException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
