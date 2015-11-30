62a1cd68444ef373ff39b992ae438df5a0b9fca3
Tanat <researcher86@mail.ru>
2015-11-30 12:48:41 +0600

    Глобальный контроллер для обработки HTTP ошибок

---
+++ src/main/java/com/tanat/shop/web/handlers/AppHTTPErrorHandler.java
@@ -0,0 +1,23 @@
+package com.tanat.shop.web.handlers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Глобальный контроллер для обработки HTTP ошибок
+ * Created by Tanat on 30.11.2015.
+ */
+@Controller
+public class AppHTTPErrorHandler {
+    @RequestMapping(value = "/404", method = RequestMethod.GET)
+    public String handle404(Model model) {
+        return "/errors/404";
+    }
+
+    @RequestMapping(value = "/unknown", method = RequestMethod.GET)
+    public String handleUnknown(Model model) {
+        return "/errors/unknown";
+    }
+}
