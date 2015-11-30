f6fa91c0fcd8a90dbf924c6708a890eb361b12e3
Tanat <researcher86@mail.ru>
2015-11-30 12:48:11 +0600

    Глобальный контроллер для обработки ошибок приложения

---
+++ src/main/java/com/tanat/shop/web/handlers/AppExceptionHandler.java
@@ -0,0 +1,45 @@
+package com.tanat.shop.web.handlers;
+
+import com.tanat.shop.exceptions.AppException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * Глобальный контроллер для обработки ошибок приложения
+ * Created by Tanat on 27.11.2015.
+ */
+@ControllerAdvice
+public class AppExceptionHandler {
+    protected Logger logger = LoggerFactory.getLogger(getClass().getName());
+
+    // Total control - setup a model and return the view name yourself. Or consider
+    // subclassing ExceptionHandlerExceptionResolver (see below).
+    @ExceptionHandler(Exception.class)
+    public ModelAndView handleError(HttpServletRequest req, Exception exception) {
+        logger.error("Request: " + req.getRequestURL() + " raised " + exception);
+
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exceptionName", exception.getClass().getCanonicalName());
+        mav.addObject("exception", exception);
+        mav.addObject("msg", "Неизвестная ошибка!");
+        mav.setViewName("/errors/error");
+        return mav;
+    }
+
+    @ExceptionHandler(AppException.class)
+    public ModelAndView handleErrorNotFound(HttpServletRequest req, Exception exception) {
+        logger.error("Request: " + req.getRequestURL() + " raised " + exception);
+
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exceptionName", exception.getClass().getCanonicalName());
+        mav.addObject("exception", exception);
+        mav.addObject("msg", exception.getMessage());
+        mav.setViewName("/errors/error");
+        return mav;
+    }
+}
