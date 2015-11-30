1d9b79922860d883cded5f5e83a66ef594b5b9b6
Tanat <researcher86@mail.ru>
2015-11-30 12:47:09 +0600

    Переименовал Exception приложения

---
+++ src/main/java/com/tanat/shop/exceptions/AppException.java
@@ -0,0 +1,16 @@
+package com.tanat.shop.exceptions;
+
+/**
+ * Exception бросается при возникновении ошибки в приложении
+ * Created by Tanat on 25.11.2015.
+ */
+public class AppException extends RuntimeException {
+    public AppException(String message) {
+        super(message);
+    }
+
+    public AppException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
