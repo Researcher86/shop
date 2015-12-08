29f29c958ef53949cdc666fec8beeb06ae3d519f
Tanat <researcher86@mail.ru>
2015-12-08 12:07:28 +0600

    Рефакторинг

--- src/main/java/com/tanat/shop/web/controller/MainController.java
+++ src/main/java/com/tanat/shop/web/controller/MainController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
+ * Контроллер главной страницы магазина
  * Created by Tanat on 16.11.2015.
  */
 @Controller
