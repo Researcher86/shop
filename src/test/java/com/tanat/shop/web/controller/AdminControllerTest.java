a78e643ef8fda4916cc9cbca8075c32a5b79cee8
Tanat <researcher86@mail.ru>
2015-12-10 11:02:21 +0600

    Тест для админ конроллера

---
+++ src/test/java/com/tanat/shop/web/controller/AdminControllerTest.java
@@ -0,0 +1,22 @@
+package com.tanat.shop.web.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Тестируем контроллер админки
+ * Created by Tanat on 10.12.2015.
+ */
+public class AdminControllerTest {
+
+    @Test
+    public void testIndex() throws Exception {
+
+    }
+
+    @Test
+    public void testLogin() throws Exception {
+
+    }
+}
