a88db3bd145d008a44b522b1baed3ce7a11c3f3c
Tanat <researcher86@mail.ru>
2016-07-06 09:25:40 +0600

    Добавить транзакцию по умолчанию для репозиториев (по Фен-шую)

--- src/main/java/com/tanat/shop/repository/CartRepository.java
+++ src/main/java/com/tanat/shop/repository/CartRepository.java
@@ -3,11 +3,11 @@ package com.tanat.shop.repository;
 import com.tanat.shop.model.Cart;
 import com.tanat.shop.model.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Repository
+@Transactional(readOnly = true)
 public interface CartRepository extends JpaRepository<Cart, Long> {
     List<Cart> findByClient(Client client);
 }
--- src/main/java/com/tanat/shop/repository/CategoryRepository.java
+++ src/main/java/com/tanat/shop/repository/CategoryRepository.java
@@ -2,7 +2,9 @@ package com.tanat.shop.repository;
 
 import com.tanat.shop.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional(readOnly = true)
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
 }
